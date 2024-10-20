49b0c0572aac985a786b072b023cf1b76c43a42b
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-10-20 22:07:00 +0200

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
+import Header from "@/components/Header/Header";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { roboto, montserrat } from "@/app/fonts/fonts";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +16,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${montserrat.variable} antialiased`}
       >
-        {children}
+      <Header/>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/app/series/page.tsx
@@ -0,0 +1,7 @@
+export default function SeriesPage(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
