5e544d1ef3c2024425125e3aeafc1fb638b1cf1a
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-10-22 00:44:14 +0200

    add style card

--- src/components/media-card/MediaCard.tsx
+++ src/components/media-card/MediaCard.tsx
@@ -7,19 +7,19 @@ interface MediaCardProps {
 
 export default function MediaCard({ mediaId }: MediaCardProps) {
   return (
-    <div>
-        <Link href={`/movies/${mediaId}`}>
-          <div>
+    <div className=" shadow-md min-w-52">
+        <Link href={`/movies/${mediaId}`} className="">
+          <div className="w-full h-72 relative ">
             <Image
               src="https://image.tmdb.org/t/p/w500/hYQs5RPHiWctoYqvI8baHiIqdd8.jpg"
               alt="media title"
-              width={500}
-              height={750}
+              fill
+              className="rounded-t-md  "
             />
           </div>
-          <div>
-            <h2>Creed II</h2>
-            <p>Le 01/03/2023</p>
+          <div className="px-5 py-2.5 ">
+            <h2 className="font-semibold my-2 text-base">Creed II</h2>
+            <p className="text-xs">Le 01/03/2023</p>
           </div>
         </Link>
     </div>
