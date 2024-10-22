2afcba5df3bc90304e1d24029338a8c4da1be054
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-10-22 23:20:20 +0200

    add popular component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${roboto.variable} ${montserrat.variable} antialiased`}
+        className={`${roboto.variable} ${montserrat.variable} antialiased bg-neutral-200`} 
       >
       <Header/>
         <main>
