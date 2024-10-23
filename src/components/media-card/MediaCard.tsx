c8be3e88fae4c1cac7d4de69460ff360d7d6ea14
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-10-23 14:05:26 +0200

    add search movie

--- src/components/media-card/MediaCard.tsx
+++ src/components/media-card/MediaCard.tsx
@@ -7,18 +7,18 @@ interface MediaCardProps {
 
 export default function MediaCard({ media }: MediaCardProps) {
   return (
-    <div className=" shadow-md min-w-48 rounded-md">
+    <div className=" shadow-md min-w-48 rounded-md z-0">
         <Link href={`/movies/${media.id}`} className="">
           <div className="w-full h-64 relative">
             <Image
-              src={`${process.env.TMDB_IMAGE_BASE_PATH}${media.poster_path}`}
+              src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_PATH}${media.poster_path}`}
               alt={media.title}
               fill
               className="rounded-t-md "
             />
           </div>
-          <div className="px-5 py-2.5 relative">
-            <p className="w-8 h-8 absolute border-1 border-gray-900 rounded-full z-10 bg-white flex justify-center items-center">{media.vote_average.toFixed(1)}</p>
+          <div className="px-5 py-2.5 relative bg-white">
+            <p className="w-8 h-8 absolute border border-black -top-4  rounded-full z-10 bg-white flex justify-center items-center">{media.vote_average.toFixed(1)}</p>
             <h3 className="font-semibold my-2 text-base">{media.title}</h3>
             <p className="text-xs">Le {new Date(media.release_date).toLocaleDateString('fr-FR')}</p>
           </div>
