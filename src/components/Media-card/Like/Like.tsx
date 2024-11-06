a070c0df746d9a67cb1565f226d8a87959499428
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-11-06 14:17:17 +0100

    add probleme mediacard

---
+++ src/components/Media-card/Like/Like.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+type LikeProps = {
+  mediaId: string;
+  isLiked: boolean; 
+  onLikeToggle: () => void; 
+  forceRed?: boolean;
+};
+
+export default function Like({ mediaId, isLiked, forceRed = false }: LikeProps) {
+  const { data: session } = useSession();
+  const [liked, setLiked] = useState(isLiked); 
+  const router= useRouter();
+
+  const handleLikeClicked = async (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    if (!session) {
+      signIn();
+      return;
+    }
+
+    await fetch(`/api/like/${mediaId}`, {
+      method: "POST",
+    });
+
+    setLiked(!liked);
+    router.refresh()
+
+  };
+
+  return (
+    <div onClick={handleLikeClicked} className="z-20 absolute -top-1 -right-1.5 cursor-pointer text-red-500 hover:text-sky-600 duration-200 ease-in-out transform">
+       <FaHeart color={forceRed || liked ? "red" : "grey"} />
+    </div>
+  );
+}
---
+++ src/components/Media-card/MediaCard.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Like from "@/components/Media-card/Like/Like";
+
+interface MediaCardProps {
+  locale: string;
+  media: Media;
+  isLiked: boolean;
+  onLikeToggle: () => void; 
+  forceRed?: boolean;
+}
+
+interface Media {
+  id: string;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+  release_date: string;
+}
+export default function MediaCard({ media, locale, isLiked, onLikeToggle,  forceRed = false }: MediaCardProps) {
+
+  return (
+    <div className="shadow-lg rounded-lg w-60 md:w-48 z-0 flex flex-col relative">
+      <Link href={`/${locale}/movies/${media.id}`}>
+        <Like mediaId={media.id} isLiked={isLiked} onLikeToggle={onLikeToggle} forceRed={forceRed} />
+        <div className="w-full aspect-[3/4] overflow-hidden relative">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_PATH}/w500${media.poster_path}`}
+            alt={media.title}
+            fill
+            className="rounded-t-md"
+          />
+        </div>
+      </Link>
+      <div className="flex flex-col justify-between flex-grow px-5 py-2.5 relative bg-white rounded-b-lg">
+        <div className="flex flex-col flex-grow">
+          <p className="w-8 h-8 absolute border border-black -top-4 rounded-full z-10 bg-white flex justify-center items-center">
+            {media.vote_average.toFixed(1)}
+          </p>
+          <h3 className="font-semibold my-2 text-base truncate max-h-10 overflow-hidden">
+            {media.title}
+          </h3>
+        </div>
+        <p className="text-xs ">
+          Le {new Date(media.release_date).toLocaleDateString("fr-FR")}
+        </p>
+      </div>
+    </div>
+  );
+}
