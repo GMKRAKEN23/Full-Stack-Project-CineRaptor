fcf78b84b551891b3f6c9cec43d45c8f925b2bfc
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-11-06 17:32:22 +0100

    add modif test

--- src/app/[locale]/movies/(search)/page.tsx
+++ src/app/[locale]/movies/(search)/page.tsx
@@ -1,24 +1,26 @@
 import SearchResults from "./SearchResults";
 
 interface MoviePageProps {
-    params: Promise<{
-        id: string;
-        locale: "en" | "fr";
-    }>;
-    searchParams: Record<string, string | undefined>;
+  params: Promise<{
+    id: string;
+    locale: "en" | "fr";
+  }>;
+  searchParams: Promise<Record<string, string | undefined>>;
 }
 
 export default async function MoviesPage({
-    params,
-    searchParams,
+  params,
+  searchParams,
 }: MoviePageProps) {
-    const { id: genreId, locale } = await params;
+  // Résoudre les promesses pour récupérer les données
+  const { id: genreId, locale } = await params;
+  const resolvedSearchParams = await searchParams;
 
-    return (
-        <SearchResults
-            searchParams={searchParams}
-            locale={locale}
-            genreId={genreId}
-        />
-    );
+  return (
+    <SearchResults
+      searchParams={resolvedSearchParams}
+      locale={locale}
+      genreId={genreId}
+    />
+  );
 }
